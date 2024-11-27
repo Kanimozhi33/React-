5544e683971b0c67d3855f7416461a07b3f2e5ea
kanimozhi33 <kanimozhi33vm@outlook.com>
2024-11-27 15:02:14 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const parent = React.createElement("div",{id: "parent"},
+     [React.createElement("div",{id: "child"},
+    [
+    React.createElement("h1",{id:"grandchild"},"hey.. hi KANIMOZHI...YOU ARE GONNA BE AWESOME "),
+    React.createElement("h2", {id:"sibling"},"this is my sibling")]),
+    React.createElement("div",{id: "child2"},
+    [
+    React.createElement("h1",{id:"maya"},"iam the second child"),
+    React.createElement("h2",{id:"isabelle"},"iam the the third child")])]);
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
